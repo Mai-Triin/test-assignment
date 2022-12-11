7a1cb09daa04267df84ffc9bf12bc4aa1aad302b
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-12-11 16:26:28 +0200

    MAI- added swagger

--- src/main/java/com/cgi/library/base/WebConfig.java
+++ src/main/java/com/cgi/library/base/WebConfig.java
@@ -3,8 +3,10 @@ package com.cgi.library.base;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
