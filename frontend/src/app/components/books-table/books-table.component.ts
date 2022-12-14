3b7070d3a000d0c9231e9f0e913fe4f4aa4bfc69
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-12-14 20:06:42 +0200

    MAI- added pagination to table of books

--- frontend/src/app/components/books-table/books-table.component.ts
+++ frontend/src/app/components/books-table/books-table.component.ts
@@ -1,8 +1,10 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {BookService} from '../../services/book.service';
 import {Observable} from 'rxjs';
 import {Page} from '../../models/page';
 import {Book} from "../../models/book";
+import {MatPaginator, PageEvent} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 
 @Component({
@@ -12,17 +14,42 @@ import {Book} from "../../models/book";
 })
 
 export class BooksTableComponent implements OnInit {
-  books$: Observable<Page<Book> | Error>;
-  displayedColumns: String[] = ['title', 'author', 'genre'];
+  books: Book [] = [];
+  displayedColumns: String[] = ['title', 'author', 'genre', 'status'];
+  totalElements: number = 0;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private bookService: BookService,
   ) {
   }
 
-  ngOnInit() {
+
+  ngOnInit(): void {
     // TODO this observable should emit books taking into consideration pagination, sorting and filtering options.
-    this.books$ = this.bookService.getBooks({});
+    this.getBooks({pageIndex: "0", pageSize: "20"})
+
   }
 
+  private getBooks(request) {
+    this.bookService.getBooks(request)
+      .subscribe(data => {
+          this.books = data['content'];
+          this.totalElements = data['totalElements'];
+        }
+        , error => {
+          console.log(error.error.message);
+        }
+      );
+  }
+
+  nextPage(event: PageEvent) {
+    const request = {};
+    request['pageIndex'] = event.pageIndex.toString();
+    request['pageSize'] = event.pageSize.toString();
+
+    this.getBooks(request);
+  }
 }
