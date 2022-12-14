6f87f38d612639075fd86aae4623820a0b0b942b
Mai-Triin <110418700+Mai-Triin@users.noreply.github.com>
2022-12-14 21:19:16 +0200

    MAI- fixed sorting and pagination in table of books

--- frontend/src/app/components/books-table/books-table.component.ts
+++ frontend/src/app/components/books-table/books-table.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {BookService} from '../../services/book.service';
 import {Book} from "../../models/book";
-import {MatPaginator, PageEvent} from "@angular/material/paginator";
-import {MatSort, Sort} from "@angular/material/sort";
+import {PageEvent} from "@angular/material/paginator";
+import {Sort} from "@angular/material/sort";
 
 
 @Component({
@@ -13,11 +13,12 @@ import {MatSort, Sort} from "@angular/material/sort";
 
 export class BooksTableComponent implements OnInit {
   books: Book [] = [];
+  pageIndex: string;
+  pageSize: string;
+  sort: string;
+  direction: string;
   displayedColumns: String[] = ['title', 'author', 'genre', 'status'];
   totalElements: number = 0;
-
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
 
   constructor(
     private bookService: BookService,
@@ -45,15 +46,23 @@ export class BooksTableComponent implements OnInit {
 
   nextPage(event: PageEvent) {
     const request = {};
+    this.pageIndex = event.pageIndex.toString();
+    this.pageSize = event.pageSize.toString();
     request['pageIndex'] = event.pageIndex.toString();
     request['pageSize'] = event.pageSize.toString();
+    request['sort'] = this.sort;
+    request['direction'] = this.direction;
     this.getBooks(request);
   }
 
   announceSortChange(event: Sort) {
     const request = {};
+    this.sort = event.active;
+    this.direction = event.direction;
     request['sort'] = event.active;
     request['direction'] = event.direction;
+    request['pageIndex'] = this.pageIndex;
+    request['pageSize'] = this.pageSize;
     this.getBooks(request);
   }
 }
